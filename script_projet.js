0ccf2f7620d2f00c7bd62f02575ef81d988375e2
Lou-AnneDub <dubillelouanne@gmail.com>
2023-12-14 22:58:06 +0100

    :construction: amène à page d'erreur

--- script_projet.js
+++ script_projet.js
@@ -18,58 +18,68 @@ document.addEventListener("DOMContentLoaded", function () {
 
             console.log('Id =',id);
 
-            d3.select('.preview')
-                .selectAll('img')
-                .data(data[id].Img)
-                .join('img')
-                .attr('class','previewImg')
-                .attr('src', function (d) { return 'img/' + d; })
-                .attr('alt', "Voir l'image ");
+            if (data[id]) {
+                d3.select('.preview')
+                    .selectAll('img')
+                    .data(data[id].Img)
+                    .join('img')
+                    .attr('class','previewImg')
+                    .attr('src', function (d) { return 'img/' + d; })
+                    .attr('alt', "Voir l'image ");
 
-            d3.select('.title h1').text(data[id].Title);
+                d3.select('.title h1').text(data[id].Title);
 
-            d3.select('#date').text(data[id].Date);
+                d3.select('#date').text(data[id].Date);
 
-            d3.select('#lienProjet')
-                .attr('href', function (d) { return data[id].Lien1;})
-                .attr('target', '_blank');
+                d3.select('#lienProjet')
+                    .attr('href', function (d) { return data[id].Lien1;})
+                    .attr('target', '_blank');
 
-            if (data[id].Lien2) {
-                // Crée le lien "bonus" seulement s'il existe
-                d3.select('#lienBonus')
-                    .attr('href', data[id].Lien2)
-                    .text('Lien Bonus');
-            } else {
-                // S'il n'existe pas, cache le lien "bonus"
-                d3.select('#lienBonus').style('display', 'none');
-            }
+                if (data[id].Lien2) {
+                    // Crée le lien "bonus" seulement s'il existe
+                    d3.select('#lienBonus')
+                        .attr('href', data[id].Lien2)
+                        .text('Lien Bonus');
+                } else {
+                    // S'il n'existe pas, cache le lien "bonus"
+                    d3.select('#lienBonus').style('display', 'none');
+                }
 
-            d3.select('#description').text(data[id].Description);
+                d3.select('#description').text(data[id].Description);
 
-            d3.select('#explication').text(data[id].Text);
+                d3.select('#explication').text(data[id].Text);
 
-            d3.select('#technologie ul')
-                .selectAll('li')
-                .data(data[id].Techno)
-                .join('li')
-                .text(function (d) { return d; });
+                d3.select('#technologie ul')
+                    .selectAll('li')
+                    .data(data[id].Techno)
+                    .join('li')
+                    .text(function (d) { return d; });
 
-            d3.select('#context ul')
-                .selectAll('li')
-                .data(data[id].Context)
-                .join('li')
-                .text(function (d) { return d; });
-            
-            d3.select('#create ul')
-                .selectAll('li')
-                .data(data[id].Create)
-                .join('li')
-                .text(function (d) { return d; });
-
-            d3.select('.mockup')
-                .attr('src', function (d) { return 'img/mockup/' + data[id].Mockup1; })
-                .attr('alt', "Image de l'accueil en mockup du projet de " + data[id].Title);
+                d3.select('#context ul')
+                    .selectAll('li')
+                    .data(data[id].Context)
+                    .join('li')
+                    .text(function (d) { return d; });
                 
+                d3.select('#create ul')
+                    .selectAll('li')
+                    .data(data[id].Create)
+                    .join('li')
+                    .text(function (d) { return d; });
+
+                d3.select('.mockup')
+                    .attr('src', function (d) { return 'img/mockup/' + data[id].Mockup1; })
+                    .attr('alt', "Image de l'accueil en mockup du projet de " + data[id].Title);
+                    
+            } else {
+                window.location.replace('/404.html');
+            }
         })
+
+        .catch(function (error) {
+            console.error('Erreur lors du chargement des données JSON :', error);
+            // Redirige l'utilisateur vers la page 404
+            window.location.replace('/404.html');
+        });
 
 });
