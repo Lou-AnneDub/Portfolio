9d4af76dd74dde6b9105d62174db2c086d36d6a3
Lou-AnneDub <dubillelouanne@gmail.com>
2024-04-19 14:12:31 +0200

    :bug:

--- script_accueil.js
+++ script_accueil.js
@@ -63,14 +63,14 @@ document.addEventListener("DOMContentLoaded", function () {
       
         // Navigation Slider
         let translateValue = 0;
-        let slideWidth = 20; 
+        let slideWidth = 16; 
 
         //La largeur de l'écran
         const largeurEcran = window.innerWidth;
 
         //Modifier la valeur de la variable en fonction de la largeur de l'écran
         if (largeurEcran < 700) {
-            slideWidth = 20;
+            slideWidth = 16;
         } else {
             slideWidth = 13;
         }
